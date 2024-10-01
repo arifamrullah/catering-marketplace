8143e094f6f0eaf1b075626f0a7944e1cd82d001
Arif Amrullah <aram@Arifs-MacBook-Pro.local>
2024-10-01 16:06:49 +0700

    added breeze (custom auth)

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:'.User::class],
+            'role' => ['required', 'integer'],
+            // 'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'role' => $request->role,
+            // 'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        $authUserRole = Auth::user()->role;
+
+        if($authUserRole == 0) {
+            return redirect(route('admin', absolute: false));
+        } elseif ($authUserRole == 1) {
+            return redirect(route('merchant', absolute: false));
+        } else {
+            return redirect(route('customer', absolute: false));
+        }
+
+        // return redirect(route('dashboard', absolute: false));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin');
+})->middleware(['auth', 'verified','rolemanager:admin'])->name('admin');
+
+Route::get('/merchant/dashboard', function () {
+    return view('merchant');
+})->middleware(['auth', 'verified','rolemanager:merchant'])->name('merchant');
+
+Route::get('/customer/dashboard', function () {
+    return view('customer');
+})->middleware(['auth', 'verified','rolemanager:customer'])->name('customer');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
